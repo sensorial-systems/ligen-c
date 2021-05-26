961c464541a9e5e41e04dbe0d0c4b2f788ba0051
arrudagates <arrudagates@gmail.com>
2021-05-26 16:43:20 -0300

    added cfg conditional macros

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,6 +1,6 @@
 //! C Binding Generator.
 
-#![feature(proc_macro_span)]
+#![cfg_attr(cargo_ligen, feature(proc_macro_span))]
 #![warn(missing_copy_implementations)]
 #![warn(missing_debug_implementations)]
 #![warn(missing_docs)]
@@ -17,15 +17,23 @@ use proc_macro::TokenStream;
 /// Entry point for ligen_c
 #[proc_macro_attribute]
 pub fn ligen_c(args: TokenStream, input: TokenStream) -> TokenStream {
-    let source_file = proc_macro::Span::call_site().source_file();
-    let source_file = SourceFile {
-        is_real: source_file.is_real(),
-        path: source_file.path(),
-    };
-    let arguments = Arguments::from_env().expect("Failed to get the arguments");
-    let context = Context {
-        source_file,
-        arguments,
-    };
-    ligen_c_core::ligen_c(context, args.into(), input.into()).into()
+    #[cfg(cargo_ligen)]
+    {
+        let source_file = proc_macro::Span::call_site().source_file();
+        let source_file = SourceFile {
+            is_real: source_file.is_real(),
+            path: source_file.path(),
+        };
+        let arguments = Arguments::from_env().expect("Failed to get the arguments");
+        let context = Context {
+            source_file,
+            arguments,
+        };
+        ligen_c_core::ligen_c(context, args.into(), input.into()).into()
+    }
+
+    #[cfg(not(cargo_ligen))]
+    {
+        TokenStream::new()
+    }
 }
